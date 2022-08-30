f9e3334a33349928409f8cf06d7cc913835330c2
N1MBER <socialfox33@gmail.com>
2022-08-30 12:11:03 +0300

    docs(documentation): add doc for rc-table stand
    consta-design-system/uikit#2568

--- src/mixs/RcTable/__stand__/RcTable.stand.tsx
+++ src/mixs/RcTable/__stand__/RcTable.stand.tsx
@@ -1,10 +1,10 @@
 import { createStand } from '##/stand/standConfig';
 
 export default createStand({
-  title: 'RcTableAdapter',
-  id: 'RcTableAdapter',
-  group: 'adapters',
-  description: 'Этот адаптер стилизует таблицу.',
+  title: 'RcTable',
+  id: 'RcTable',
+  group: 'mixs',
+  description: 'Миксин для стилизации таблицы.',
   version: '1.0.5',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
