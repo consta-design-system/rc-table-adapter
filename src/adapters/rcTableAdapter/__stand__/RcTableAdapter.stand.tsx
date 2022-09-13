927671a6ec3512bc877b073a61d2147ada6f1df8
N1MBER <socialfox33@gmail.com>
2022-09-13 17:27:59 +0300

    docs(sandbox): add sandboxes links
    consta-design-system/uikit#2615

--- src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
+++ src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
@@ -6,6 +6,8 @@ export default createStand({
   group: 'adapters',
   description: 'Этот адаптер стилизует таблицу.',
   version: '1.0.5',
+  sandbox:
+    'https://codesandbox.io/embed/rctableadapter-qm5648?fontsize=14&hidenavigation=1&theme=dark',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
--- src/mixs/RcTable/__stand__/RcTable.stand.tsx
+++ src/mixs/RcTable/__stand__/RcTable.stand.tsx
@@ -6,6 +6,8 @@ export default createStand({
   group: 'mixs',
   description: 'Миксин для стилизации таблицы.',
   version: '1.0.5',
+  sandbox:
+    'https://codesandbox.io/embed/cnrctable-440ur7?fontsize=14&hidenavigation=1&theme=dark',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
