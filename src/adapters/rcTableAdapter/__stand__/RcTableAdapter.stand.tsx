36cba2b8e0ac191cae5e3f9c208e32f0515b41ff
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-09-15 14:37:19 +0300

    docs(sandbox): add sandboxes links (#25)
    * docs(sandbox): add sandboxes links
    
    consta-design-system/uikit#2615
    
    * fix(sandbox): rewrite links to ids
    
    :w
    
    * Update yarn.lock
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
+++ src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
@@ -6,6 +6,7 @@ export default createStand({
   group: 'adapters',
   description: 'Этот адаптер стилизует таблицу.',
   version: '1.0.5',
+  sandbox: 'rctableadapter-qm5648',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
--- src/components/ExpandIcon/__stand__/ExpandIcon.stand.tsx
+++ src/components/ExpandIcon/__stand__/ExpandIcon.stand.tsx
@@ -6,6 +6,7 @@ export default createStand({
   group: 'components',
   description: 'Иконка для скрытых элементов.',
   version: '1.0.5',
+  sandbox: 'expandicon-ns75lc',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
