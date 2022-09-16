db01ce164f01fa68bf9496b70fc0d0dd3383825f
gizeasy <gizeasy@gmail.com>
2022-09-16 10:42:23 +0300

    Docs/sandbox (#26)
    * docs(documentation): add doc for rc-table stand
    
    consta-design-system/uikit#2568
    
    * docs(sandbox): add sandboxes links
    
    consta-design-system/uikit#2615
    
    * fix(sandbox): rewrite links to ids
    
    :w
    
    * Update yarn.lock
    
    * Update RcTableAdapter.stand.tsx
    
    Co-authored-by: N1MBER <socialfox33@gmail.com>

--- src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
+++ src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
@@ -1,14 +1,12 @@
 import { createStand } from '##/stand/standConfig';
 
-import image from './RcTableAdapter.image.svg';
-
 export default createStand({
   title: 'RcTableAdapter',
   id: 'RcTableAdapter',
-  image,
   group: 'adapters',
   description: 'Этот адаптер стилизует таблицу.',
   version: '1.0.5',
+  sandbox: 'rctableadapter-qm5648',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
--- src/mixs/RcTable/__stand__/RcTable.stand.tsx
+++ src/mixs/RcTable/__stand__/RcTable.stand.tsx
@@ -6,6 +6,7 @@ export default createStand({
   group: 'mixs',
   description: 'Миксин для стилизации таблицы.',
   version: '1.0.5',
+  sandbox: 'cnrctable-440ur7',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
