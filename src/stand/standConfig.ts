dc33cfb76bc9fbc58d244ef1632b7c165331bccf
gizeasy <gizeasy@gmail.com>
2022-10-11 16:41:43 +0300

    chore(deps): update @consta/stand

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -2,25 +2,24 @@ import { createConfig } from '@consta/stand';
 
 import image from './ConstaImage.png';
 
-const groups = [
-  {
-    title: 'Компоненты',
-    id: 'components',
-  },
-  {
-    title: 'Адаптеры',
-    id: 'adapters',
-  },
-  {
-    title: 'Миксины',
-    id: 'mixs',
-  },
-] as const;
-
 export const { createStand } = createConfig({
   title: 'Consta rc-table-adapter',
   id: 'rc-table-adapter',
-  groups,
+  groups: [
+    {
+      title: 'Компоненты',
+      id: 'components',
+    },
+    {
+      title: 'Адаптеры',
+      id: 'adapters',
+      view: 'card',
+    },
+    {
+      title: 'Миксины',
+      id: 'mixs',
+    },
+  ],
   group: 'Адаптеры',
   image,
   description: 'Адаптер для таблицы rc-table.',
