87d6531946017a4f9a75f00545994a3b66e36966
Maxim Kolesnikov <socialfox33@gmail.com>
2023-04-18 13:59:45 +0300

    docs(rc-table-adapter): add aliases
    consta-design-system/uikit#2955

--- src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
+++ src/adapters/rcTableAdapter/__stand__/RcTableAdapter.stand.tsx
@@ -6,6 +6,7 @@ export default createStand({
   group: 'adapters',
   description: 'Этот адаптер стилизует таблицу.',
   version: '1.0.0',
+  alias: ['таблица', 'адаптер', 'adapter', 'стилизация'],
   sandbox: 'rctableadapter-qm5648',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
