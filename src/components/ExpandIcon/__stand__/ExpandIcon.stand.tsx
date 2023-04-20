5381ad3b61c4269e2b7c0a8852c25b8efd41628a
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-04-20 18:31:30 +0300

    docs(rc-table-adapter): add aliases (#33)
    consta-design-system/uikit#2955

--- src/components/ExpandIcon/__stand__/ExpandIcon.stand.tsx
+++ src/components/ExpandIcon/__stand__/ExpandIcon.stand.tsx
@@ -7,6 +7,7 @@ export default createStand({
   description: 'Иконка для скрытых элементов.',
   version: '1.0.0',
   sandbox: 'expandicon-ns75lc',
+  alias: ['иконка', 'вложенность', 'скрыть'],
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D70%253A13140',
   status: 'stable',
